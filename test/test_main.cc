356e25a54a42f3b027f6f11cc687c564a473fa65
Nathan Moinvaziri <nathan@nathanm.com>
2022-01-02 17:17:55 -0800

    Implement unit testing using Google Test framework.

---
+++ test/test_main.cc
@@ -0,0 +1,20 @@
+/* test_main.cc - Main entry point for google tests */
+
+#include "zbuild.h"
+#ifdef ZLIB_COMPAT
+#  include "zlib.h"
+#else
+#  include "zlib-ng.h"
+#endif
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
