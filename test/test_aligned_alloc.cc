4f1c568093e857ddc30261bb24243474b0d2ebd8
Nathan Moinvaziri <nathan@nathanm.com>
2022-06-25 15:25:22 -0700

    Prefix shared functions to prevent symbol conflict when linking native api against compat api.

--- test/test_aligned_alloc.cc
+++ test/test_aligned_alloc.cc
@@ -1,4 +1,4 @@
-/* test_aligned_alloc.cc - Test zng_alloc_aligned and zng_free_aligned */
+/* test_aligned_alloc.cc - Test zalloc_aligned and zfree_aligned */
 
 #include <stdlib.h>
 #include <errno.h>
@@ -39,8 +39,8 @@ void zng_cfree_unaligned(void *opaque, void *ptr) {
 }
 
 TEST(zalloc, aligned_64) {
-    void *return_ptr = zng_alloc_aligned(zng_calloc_unaligned, 0, 1, 100, 64);
+    void *return_ptr = PREFIX(zalloc_aligned)(zng_calloc_unaligned, 0, 1, 100, 64);
     ASSERT_TRUE(return_ptr != NULL);
     EXPECT_EQ((intptr_t)return_ptr % 64, 0);
-    zng_free_aligned(zng_cfree_unaligned, 0, return_ptr);
+    PREFIX(zfree_aligned)(zng_cfree_unaligned, 0, return_ptr);
 }
